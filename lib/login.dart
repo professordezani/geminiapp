0c0b647ca2407fe91c45dbf3076bc730e6d253d4
professorhenriquedezani <professorhenriquedezani@gmail.com>
2024-08-29 11:45:23 +0000

    tela de login

--- lib/login.dart
+++ lib/login.dart
@@ -4,7 +4,38 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Login"),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 6),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("New User"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
