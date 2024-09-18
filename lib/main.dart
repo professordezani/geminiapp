068f68e8cdfa80351dfa99779f7abf674d87014c
professorhenriquedezani <professorhenriquedezani@gmail.com>
2024-09-18 11:35:01 +0000

    add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/firebase_options.dart';
 
 import 'app.dart';
 
+// https://firebase.google.com/docs/flutter/setup?hl=pt-br&platform=ios
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(App());
 }
